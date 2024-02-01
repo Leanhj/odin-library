404c79fa9d2bd11c4b5c368c608e1245235e0744
lhiga <lhiga@dc.uba.ar>
2024-01-31 22:13:21 -0300

    Added new book functionality

--- javascript.js
+++ javascript.js
@@ -1,5 +1,7 @@
 const myLibrary = [];
 
+// --- Book object constructor ---
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -14,12 +16,19 @@ function Book(title, author, pages, read) {
     };
 }
 
+// --- Add book to library function ---
+
 function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
+// --- Display books in page function ---
+
 function displayBooks() {
     const libraryDiv = document.querySelector(".library");
+    const booksArray = document.createElement("div");
+    booksArray.className = "books-array";
+
     myLibrary.forEach(element => {
         const card = document.createElement("div");
         card.className = "card";
@@ -39,9 +48,52 @@ function displayBooks() {
         card.appendChild(cardAuthor);
         card.appendChild(cardPages);
         card.appendChild(cardRead);
-        libraryDiv.appendChild(card);
+        booksArray.appendChild(card);
     });
+
+    libraryDiv.replaceChildren(booksArray);
 }
+
+// --- New book dialog ---
+
+const newBookButton = document.querySelector(".new-book");
+const dialog = document.querySelector(".dialog");
+
+newBookButton.addEventListener("click", () => {
+    dialog.showModal();
+});
+
+const cancelButton = document.querySelector(".cancel");
+
+cancelButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    dialog.close();
+});
+
+const form = document.getElementById("form");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const myFormData = new FormData(e.target);
+
+    const formDataObj = Object.fromEntries(myFormData.entries());
+    console.log(formDataObj);
+    // dialog.close();
+
+    let readValue;
+    if (formDataObj.hasOwnProperty("book-read")) {
+        readValue = true;
+    } else {
+        readValue = false;
+    }
+    const newBook = new Book(formDataObj["book-title"],
+                            formDataObj["book-author"],
+                            formDataObj["book-pages"],
+                            readValue);
+
+    myLibrary.push(newBook);
+    displayBooks();
+});
 
 const book1 = new Book("Artemis Fowl", "Eoin Colfer", 250, true);
 const book2 = new Book("The Hobbit", "J.R.R. Tolkien", 400, false);
