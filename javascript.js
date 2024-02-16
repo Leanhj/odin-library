b0ebcc21dbc617ac8dc4e2c79dd7d77f4fc4035b
lhiga <lhiga@dc.uba.ar>
2024-02-16 17:44:49 -0300

    Changed from factories to classes

--- javascript.js
+++ javascript.js
@@ -2,19 +2,40 @@ const myLibrary = [];
 
 // --- Book object constructor ---
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function() {
+// function Book(title, author, pages, read) {
+//     this.title = title;
+//     this.author = author;
+//     this.pages = pages;
+//     this.read = read;
+//     this.info = function() {
+//         if (this.read) {
+//             return `${this.title} by ${this.author}, ${this.pages} pages, read`;
+//         } else {
+//             return `${this.title} by ${this.author}, ${this.pages} pages, not read yet`;
+//         }
+//     };
+//     this.readStatus = function() {
+//         this.read = !(this.read);
+//     }
+// }
+
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+
+    info() {
         if (this.read) {
             return `${this.title} by ${this.author}, ${this.pages} pages, read`;
         } else {
             return `${this.title} by ${this.author}, ${this.pages} pages, not read yet`;
         }
-    };
-    this.readStatus = function() {
+    }
+
+    readStatus() {
         this.read = !(this.read);
     }
 }
